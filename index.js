718055c832160d91b6adf2100464692fff25e260
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-09 19:16:15 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-#!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-
-var app = require('./app');
+var express = require('express');
+var path = require('path');
 var http = require('http');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 
 var port = process.env.PORT || '3000';
 app.set('port', port);
